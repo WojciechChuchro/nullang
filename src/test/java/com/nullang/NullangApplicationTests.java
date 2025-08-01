0b174c2523b65832fd2a64fca1f459850d38530e
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2025-08-01 18:20:31 +0200

    init

---
+++ src/test/java/com/nullang/NullangApplicationTests.java
@@ -0,0 +1,11 @@
+package com.nullang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NullangApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
