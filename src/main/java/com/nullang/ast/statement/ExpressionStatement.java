13d1be859844975f96882b1f0340d29955b72cbc
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2025-08-03 20:31:55 +0200

    add expression statement

---
+++ src/main/java/com/nullang/ast/statement/ExpressionStatement.java
@@ -0,0 +1,27 @@
+package com.nullang.ast.statement;
+
+import com.nullang.ast.Expression;
+import com.nullang.ast.Node;
+import com.nullang.ast.Statement;
+import com.nullang.token.Token;
+
+public class ExpressionStatement implements Statement{
+    private final Token token;
+    private final Expression expression;
+
+    public ExpressionStatement(Token token, Expression expression) {
+        this.token = token;
+        this.expression = expression;
+    }
+
+    @Override
+    public String tokenLiteral() {
+        return token.literal;
+    }
+
+    @Override
+    public Node StatementNode() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'StatementNode'");
+    }
+}
