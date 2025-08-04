e86d92aad3dfdd8f77344438acddab9c9f739e4a
Wojciech Chuchro <82377967+WojciechChuchro@users.noreply.github.com>
2025-08-04 21:43:06 +0200

    add new test cases

--- src/test/java/com/nullang/parser/ParserTest.java
+++ src/test/java/com/nullang/parser/ParserTest.java
@@ -17,216 +17,224 @@ import com.nullang.lexer.Lexer;
 import com.nullang.parser.errors.ParserException;
 
 import org.junit.jupiter.api.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
 
 public class ParserTest {
-    private final Logger log = LoggerFactory.getLogger(ParserTest.class);
+    private Program parseInput(String input) throws IOException {
+        Reader reader = new StringReader(input);
+        try (Lexer lexer = new Lexer(reader);
+                Parser parser = new Parser(lexer)) {
+            return parser.parseProgram();
+        }
+    }
+
+    private ParserException parseInputExpectingException(String input) throws IOException {
+        Reader reader = new StringReader(input);
+        try (Lexer lexer = new Lexer(reader);
+                Parser parser = new Parser(lexer)) {
+            return assertThrowsExactly(ParserException.class, parser::parseProgram);
+        }
+    }
 
     @Test
-    public void testLetStatements() {
-        Reader input =
-                new StringReader(
+    void testeLetStatements() throws IOException {
+        Program program =
+                parseInput(
                         """
                         let x = 5;
                         let y = 10;
                         let foobar = 838383;
                         """);
 
-        try (Lexer lexer = new Lexer(input);
-                Parser parser = new Parser(lexer); ) {
+        assertNotNull(program);
+        assertEquals(3, program.statements.size());
 
-            Program program = parser.parseProgram();
-            assertNotNull(program, "parseProgram() returned null");
-
-            var statements = program.statements;
-            assertEquals(3, statements.size(), "Expected 3 let statements");
-
-            String[] expectedIdentifiers = {"x", "y", "foobar"};
-
-            for (int i = 0; i < expectedIdentifiers.length; i++) {
-                Statement stmt = statements.get(i);
-                assertTrue(stmt instanceof LetStatement, "Statement is not a LetStatement");
-
-                LetStatement letStmt = (LetStatement) stmt;
-                assertEquals("let", letStmt.tokenLiteral());
-
-                // TODO:
-                // Identifier name = letStmt.name;
-                // assertNotNull(name, "LetStatement.name is null");
-                // assertEquals(expectedIdentifiers[i], name.getValue());
-                // assertEquals(expectedIdentifiers[i], name.tokenLiteral());
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
+        String[] expectedIdentifiers = {"x", "y", "foobar"};
+        for (int i = 0; i < expectedIdentifiers.length; i++) {
+            Statement stmt = program.statements.get(i);
+            assertTrue(stmt instanceof LetStatement, "Statement " + i + " should be LetStatement");
+            assertEquals("let", stmt.tokenLiteral());
         }
     }
 
     @Test
-    public void testReturn() {
-        Reader input =
-                new StringReader(
+    void testReturnStatements() throws IOException {
+        Program program =
+                parseInput(
                         """
                         return 5;
                         return true;
                         """);
 
-        try (Lexer lexer = new Lexer(input);
-                Parser parser = new Parser(lexer); ) {
+        assertNotNull(program);
+        assertEquals(2, program.statements.size());
 
-            Program program = parser.parseProgram();
-            assertNotNull(program, "parseProgram() returned null");
-
-            var statements = program.statements;
-            assertEquals(2, statements.size(), "Expected 2 return statements");
-
-            String[] expectedIdentifiers = {"5", "true"};
-
-            for (int i = 0; i < expectedIdentifiers.length; i++) {
-                Statement stmt = statements.get(i);
-                assertTrue(stmt instanceof ReturnStatement, "Statement is not a LetStatement");
-
-                ReturnStatement returnStmt = (ReturnStatement) stmt;
-                assertEquals("return", returnStmt.tokenLiteral());
-
-                // TODO:
-                // Identifier name = letStmt.name;
-                // assertNotNull(name, "LetStatement.name is null");
-                // assertEquals(expectedIdentifiers[i], name.getValue());
-                // assertEquals(expectedIdentifiers[i], name.tokenLiteral());
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
+        for (Statement stmt : program.statements) {
+            assertTrue(stmt instanceof ReturnStatement);
+            assertEquals("return", stmt.tokenLiteral());
         }
     }
 
     @Test
-    public void testForAssign() {
-        Reader input =
-                new StringReader(
+    void testLetStatementMissingAssignment() throws IOException {
+        ParserException ex = parseInputExpectingException("let x;");
+        assertEquals(
+                "Expected '=' after identifierToken [type=SEMICOLON, literal=;]", ex.getMessage());
+    }
+
+    @Test
+    void testLetStatementMissingIdentifier() throws IOException {
+        ParserException ex = parseInputExpectingException("let =;");
+        assertEquals(
+                "Peek should be variable name!Token [type=ASSIGN, literal==]", ex.getMessage());
+    }
+
+    @Test
+    void testIdentifierExpression() throws IOException {
+        Program program = parseInput("foobar;");
+
+        assertNotNull(program);
+        assertEquals(1, program.statements.size());
+
+        Statement stmt = program.statements.get(0);
+        assertTrue(stmt instanceof ExpressionStatement);
+
+        ExpressionStatement exprStmt = (ExpressionStatement) stmt;
+        assertEquals("foobar", exprStmt.tokenLiteral());
+        assertTrue(exprStmt.getExpression() instanceof Identifier);
+        assertEquals("foobar", ((Identifier) exprStmt.getExpression()).getValue());
+    }
+
+    @Test
+    void testIntegerExpression() throws IOException {
+        Program program = parseInput("5;");
+
+        assertNotNull(program);
+        assertEquals(1, program.statements.size());
+
+        Statement stmt = program.statements.get(0);
+        assertTrue(stmt instanceof ExpressionStatement);
+
+        ExpressionStatement exprStmt = (ExpressionStatement) stmt;
+        assertTrue(exprStmt.getExpression() instanceof IntegerIdentifier);
+        assertEquals("5", exprStmt.tokenLiteral());
+        assertEquals(5, ((IntegerIdentifier) exprStmt.getExpression()).getValue());
+    }
+
+    @Test
+    void testMixedExpressions() throws IOException {
+        Program program =
+                parseInput(
                         """
-                        let x;
+                        foobar;
+                        5;
                         """);
 
-        try (Lexer lexer = new Lexer(input);
-                Parser parser = new Parser(lexer); ) {
+        assertNotNull(program);
+        assertEquals(2, program.statements.size());
 
-            ParserException ex =
-                    assertThrowsExactly(ParserException.class, () -> parser.parseProgram());
+        // Test identifier expression
+        Statement stmt1 = program.statements.get(0);
+        assertTrue(stmt1 instanceof ExpressionStatement);
+        ExpressionStatement exprStmt1 = (ExpressionStatement) stmt1;
+        assertEquals("foobar", ((Identifier) exprStmt1.getExpression()).getValue());
 
-            assertEquals(
-                    "Expected '=' after identifierToken [type=SEMICOLON, literal=;]",
-                    ex.getMessage());
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+        // Test integer expression
+        Statement stmt2 = program.statements.get(1);
+        assertTrue(stmt2 instanceof ExpressionStatement);
+        ExpressionStatement exprStmt2 = (ExpressionStatement) stmt2;
+        assertEquals(5, ((IntegerIdentifier) exprStmt2.getExpression()).getValue());
     }
 
     @Test
-    public void testForIdent() {
-        Reader input =
-                new StringReader(
-                        """
-                        let =;
-                        """);
+    void testBangPrefixExpression() throws IOException {
+        Program program = parseInput("!5;");
 
-        try (Lexer lexer = new Lexer(input);
-                Parser parser = new Parser(lexer); ) {
+        assertNotNull(program);
+        assertEquals(1, program.statements.size());
 
-            ParserException ex =
-                    assertThrowsExactly(ParserException.class, () -> parser.parseProgram());
+        Statement stmt = program.statements.get(0);
+        assertTrue(stmt instanceof ExpressionStatement);
 
-            assertEquals(
-                    "Peek should be variable name!Token [type=ASSIGN, literal==]", ex.getMessage());
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+        ExpressionStatement exprStmt = (ExpressionStatement) stmt;
+        assertTrue(exprStmt.getExpression() instanceof PrefixExpression);
+
+        PrefixExpression prefixExpr = (PrefixExpression) exprStmt.getExpression();
+        assertEquals("!", prefixExpr.getOperator());
+        assertEquals("!", prefixExpr.tokenLiteral());
+
+        assertNotNull(prefixExpr.getRight());
+        assertTrue(prefixExpr.getRight() instanceof IntegerIdentifier);
+
+        IntegerIdentifier right = (IntegerIdentifier) prefixExpr.getRight();
+        assertEquals(5, right.getValue());
+        assertEquals("5", right.tokenLiteral());
     }
 
     @Test
-    public void testExpressions() {
-        Reader input =
-                new StringReader(
-                        """
-                            foobar;
-                            5;
-                        """);
+    void testMinusPrefixExpression() throws IOException {
+        Program program = parseInput("-5;");
 
-        try (Lexer lexer = new Lexer(input);
-                Parser parser = new Parser(lexer); ) {
+        assertNotNull(program);
+        assertEquals(1, program.statements.size());
 
-            Program program = parser.parseProgram();
-            assertNotNull(program, "parseProgram() returned null");
+        Statement stmt = program.statements.get(0);
+        assertTrue(stmt instanceof ExpressionStatement);
 
-            var statements = program.statements;
-            assertEquals(2, statements.size(), "Expected 2 statements");
+        ExpressionStatement exprStmt = (ExpressionStatement) stmt;
+        assertTrue(exprStmt.getExpression() instanceof PrefixExpression);
 
-            Statement stmt = statements.get(0);
-            assertTrue(
-                    stmt instanceof ExpressionStatement, "Statement is not a ExpressionStatement");
+        PrefixExpression prefixExpr = (PrefixExpression) exprStmt.getExpression();
+        assertEquals("-", prefixExpr.getOperator());
 
-            ExpressionStatement stm = (ExpressionStatement) stmt;
-            assertEquals("foobar", stm.tokenLiteral());
-            assertEquals("foobar", ((Identifier) stm.getExpression()).getValue());
-
-            Statement stmt2 = statements.get(1);
-            assertTrue(
-                    stmt2 instanceof ExpressionStatement, "Statement is not a ExpressionStatement");
-
-            ExpressionStatement stm2 = (ExpressionStatement) stmt2;
-            assertTrue(
-                    stm2.getExpression() instanceof IntegerIdentifier,
-                    "Expressions statement should be integer identifier!");
-
-            assertEquals("5", stm2.tokenLiteral());
-            assertEquals(5, ((IntegerIdentifier) stm2.getExpression()).getValue());
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+        assertTrue(prefixExpr.getRight() instanceof IntegerIdentifier);
+        assertEquals(5, ((IntegerIdentifier) prefixExpr.getRight()).getValue());
     }
 
     @Test
-    public void testPrefixExpression() {
-        Reader input =
-                new StringReader(
+    void testMultiplePrefixExpressions() throws IOException {
+        Program program =
+                parseInput(
                         """
                         !5;
-                -5;
+                        -5;
                         """);
 
-        try (Lexer lexer = new Lexer(input);
-                Parser parser = new Parser(lexer); ) {
+        assertNotNull(program);
+        assertEquals(2, program.statements.size());
 
-            Program program = parser.parseProgram();
-            assertNotNull(program, "parseProgram() returned null");
+        // Test bang prefix
+        ExpressionStatement stmt1 = (ExpressionStatement) program.statements.get(0);
+        PrefixExpression prefix1 = (PrefixExpression) stmt1.getExpression();
+        assertEquals("!", prefix1.getOperator());
 
-            var statements = program.statements;
-            assertEquals(2, statements.size(), "Expected 2 statements");
+        // Test minus prefix
+        ExpressionStatement stmt2 = (ExpressionStatement) program.statements.get(1);
+        PrefixExpression prefix2 = (PrefixExpression) stmt2.getExpression();
+        assertEquals("-", prefix2.getOperator());
+    }
 
-            Statement stmt = statements.get(0);
-            assertTrue(
-                    stmt instanceof ExpressionStatement, "Statement is not a ExpressionStatement");
+    private void assertPrefixExpression(String input, String expectedOperator, int expectedValue)
+            throws IOException {
+        Program program = parseInput(input);
 
-            ExpressionStatement stm = (ExpressionStatement) stmt;
-            assertTrue(
-                    stm.getExpression() instanceof PrefixExpression,
-                    "Statement is not a ExpressionStatement");
-            PrefixExpression st = (PrefixExpression) stm.getExpression();
-            assertEquals("!", st.tokenLiteral());
-            assertEquals("!", st.getOperator());
-            assertNotNull(st.getRight(), "Right operand should not be null");
-            assertTrue(
-                    st.getRight() instanceof IntegerIdentifier,
-                    "Right operand should be IntegerIdentifier");
-            IntegerIdentifier right = (IntegerIdentifier) st.getRight();
-            assertEquals(5, right.getValue(), "Right operand value should be 5");
-            assertEquals("5", right.tokenLiteral(), "Right operand token literal should be '5'");
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+        assertNotNull(program);
+        assertEquals(1, program.statements.size());
+
+        ExpressionStatement exprStmt = (ExpressionStatement) program.statements.get(0);
+        PrefixExpression prefixExpr = (PrefixExpression) exprStmt.getExpression();
+
+        assertEquals(expectedOperator, prefixExpr.getOperator());
+        IntegerIdentifier right = (IntegerIdentifier) prefixExpr.getRight();
+        assertEquals(expectedValue, right.getValue());
+    }
+
+    @Test
+    void testPrefixExpressionOperators() throws IOException {
+        assertPrefixExpression("!10;", "!", 10);
+        assertPrefixExpression("-15;", "-", 15);
     }
 }
